bdd7354688bb5db6d894f016d0436ef7431a61af
peraltalaura <peralta.laura@uni.eus>
2021-05-07 14:11:10 +0200

    Prueba

--- Web_Page/home.php
+++ Web_Page/home.php
@@ -1,3 +1,82 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Home</title>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+		
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<Link rel="stylesheet" href="CSS.css"></Link>
+		<link rel="icon"  type="image/jpg" href="images/bee-icon.jpg"></link>
+	</head>
+	<body class="bg-warning Cen">
+		<div class="bg-dark Center">
+			<h1 class="display-1 text-warning">ERLETE</h1>
+		</div>
+		<nav class="navbar navbar-expand-sm bg-dark sticky-top p-4">
+			<ul class="navbar-nav nav-pills flex-fill">
+				<li class="nav-item flex-fill">
+					<a class="nav-link active bg-warning text-dark Center" href="home.php">HOME</a>
+				</li>
+				<li class="nav-item dropdown flex-fill">
+					<a class="nav-link dropdown-toggle text-warning text-center" href="home.php" data-toggle="dropdown">INFORMATION</a>
+					<div class="dropdown-menu bg-dark">
+						<a class="dropdown-item text-warning" href="home.php#">Purpose</a>
+						<a class="dropdown-item text-warning" href="home.php#">How does it work?</a>
+						<a class="dropdown-item text-warning" href="home.php#">Rules</a>
+						<a class="dropdown-item text-warning" href="home.php#">How to become a member</a>
+						<a class="dropdown-item text-warning" href="home.php#">Contact us</a>
+					</div>
+				</li>
+				<li class="nav-item flex-fill">
+					<a class="nav-link text-warning Center" href="SD_EXAMPLE.html">FORUM</a>
+				</li>
+				<li class="nav-item flex-fill">
+					<a class="nav-link text-warning Center" href="SD_GOALS.html">BOOKINGS</a>
+				</li>
+				<li class="nav-item flex-fill">
+					<a class="nav-link text-warning Center" href="SD_EXAMPLE2.html">MY ACCOUNT</a>
+				</li>
+			</ul>
+		</nav>
+		<br>
+		<div class="container Center">
+			<h2 class="display-4">LEARN ABOUT OUR PURPOSE AND ACTIVITIES</h2>
+			<div class="bg-dark text-warning RB">
+				<p>Read information about how everything works and why we like being in this community, how to become a part of this and how to contact us for further information</p>
+			</div>
+			<button type="button" class="btn btn-dark text-warning BRB">GO TO INFORMATION</button>
+			<h2 class="display-4 ">SEE DIFFERENT ISSUES DISCUSSED IN OUR COMMUNITY</h2>
+			<div class="bg-dark text-warning RB">
+				<p>Everyone can enter and see the questions, answers and discussions of our forum</p>
+				<p>Members can also participate actively in the community and post comments</p>
+			</div>
+			<button type="button" class="btn btn-dark text-warning BRB">GO TO FORUM</button>
+			<h2 class="display-4 ">BOOK OUR EXTRACTOR AND METAL BINS</h2>
+			<div class="bg-dark text-warning RB">
+				<p>Only members: See de days available to book the extractor and the metalbins available to us that we provide</p>
+			</div>
+			<button type="button" class="btn btn-dark text-warning BRB">BOOK NOW</button>
+			<h2 class="display-4 ">CONSULT YOUR PERSONAL INFORMATION</h2>
+			<div class="bg-dark text-warning RB">
+				<p>Only members: Have access to your personal information and be able to modify it, make a transfer, see your bookings, productions and account status at any time</p>
+			</div>
+			<button type="button" class="btn btn-dark text-warning BRB">YOUR ACCOUNT</button>
+		</div>
+		<div class="bg-dark p-4">
+			<address></address>
+		</div>
+	</body>
+=======
 <!DOCTYPE html>
 <html>
 	<head>
@@ -105,4 +184,5 @@
 			<address></address>
 		</div>
 	</body>
+>>>>>>> 37d660a2072fc2a3febe5c01ee49526d60fa3301
 </html>			
