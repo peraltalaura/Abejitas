aa8cd717a99ca4d1226254027a21594e2812d53d
peraltalaura <75114015+peraltalaura@users.noreply.github.com>
2021-05-10 13:31:10 +0200

    Add files via upload

--- Web_Page/index.php
+++ Web_Page/index.php
@@ -18,7 +18,7 @@
 		<!-- Latest compiled JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<Link rel="stylesheet" href="CSS.css">
+		<Link rel="stylesheet" href="index_CSS.css">
 		
 		<style>
 			body {
