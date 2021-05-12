610f27607b0f8009ca3686dd665a0d4d0626c3c4
peraltalaura <peralta.laura@uni.eus>
2021-05-12 09:20:34 +0200

    update fixed

--- Web_Page/update_data.php
+++ Web_Page/update_data.php
@@ -1,7 +1,8 @@
 <?php
 	include("test_connect_db.php");
 	$link=connectDataBase();
-
+	session_start();
+	
 	$user=$_POST['uname'];
 	$surname=$_POST['usurname'];
 	$email=$_POST['mail'];
@@ -14,9 +15,8 @@
 	
 	$sql="UPDATE member SET name='$user', surname='$surname', email='$email', birthdate='$birth', city='$city', address='$address', phone=$phone WHERE member_id=$id";
 	mysqli_query($link,$sql);
-	
 	if(mysqli_query($link,$sql)){
-		$_SESSION['globaluser']=$user;
+		$id=$_SESSION['memberID'];
 		header("Location:my_account.php ? update=yes");
 	} else {
 		header("Location:my_account.php ? update=no");
