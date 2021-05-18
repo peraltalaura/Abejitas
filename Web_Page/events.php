8aedabda85558948b11efbc60269d978e0d7090f
MarkelUribe <75306077+MarkelUribe@users.noreply.github.com>
2021-05-18 09:32:52 +0200

    Add files via upload

---
+++ Web_Page/events.php
@@ -0,0 +1,10 @@
+<?php 
+	header('Content-Type: application/json');
+	include('test_connect_db.php');
+	$pdo = new PDO("mysql:dbname=erlete_db;host=localhost","root","dam1");
+	$sentenciaSQL = $pdo->prepare("SELECT booking_id as id, entrydate as start, exitdate as end, name as title FROM booking INNER JOIN member ON booking.member_id = member.member_id");
+	$sentenciaSQL->execute();
+
+	$resultado= $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($resultado);
+ ?>
