0fef4304855af9bc90789377628510e8fd0926b2
MarkelUribe <75306077+MarkelUribe@users.noreply.github.com>
2021-05-18 11:04:27 +0200

    Add files via upload

--- Web_Page/book.php
+++ Web_Page/book.php
@@ -7,9 +7,38 @@
 	$exit=$_POST['exit'];
 	$id=$_SESSION['memberID'];
 	
-	$sql="INSERT INTO booking(entrydate,exitdate,member_id,kilos,total) VALUES('$entry','$exit',$id,0,0)";
+	$sql="SELECT entrydate, exitdate FROM booking";
+	$result = mysqli_query($link,$sql);
+	$ok = true;
+	if($entry<$exit){
+		while($dayc=mysqli_fetch_array($result)){
+			$p1=-1;
+			$p2=-2;
+			if($dayc['entrydate']<$exit){
+				$p1=1;
+			}else{
+				$p1=0;
+			}
+			if($dayc['exitdate']<$entry){
+				$p2=1;
+			}else{
+				$p2=0;
+			}
+			if($p1!=$p2){
+				$ok=false;
+			}
+		}
+	}else{
+		$ok=false;
+	}
+
+	if($ok==true){
+		$sql="INSERT INTO booking(entrydate,exitdate,member_id,kilos,total) VALUES('$entry','$exit',$id,0,0)";
+	}else{
+		$sql="SELECT * FROM member";
+	}
 	
-	if(mysqli_query($link,$sql)){
+	if(mysqli_query($link,$sql) && $ok == true){
 		$id=$_SESSION['memberID'];
 		header("Location:bookings.php?insert=yes");
 		} else {
