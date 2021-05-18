c41d360a5d980d7bf2a422813a06a2d0f6a17a82
MarkelUribe <75306077+MarkelUribe@users.noreply.github.com>
2021-05-18 13:45:44 +0200

    Add files via upload

--- Web_Page/book.php
+++ Web_Page/book.php
@@ -5,11 +5,47 @@
 	
 	$entry=$_POST['entry'];
 	$exit=$_POST['exit'];
+	$exit = date('Y-m-d', strtotime($exit. ' + 1 days'));
 	$id=$_SESSION['memberID'];
+	$ok=true;
+
+	$cdate=date("Y-m-d");
+
+	$sql="SELECT entrydate, exitdate FROM booking";
+	$result = mysqli_query($link,$sql);
+	if($exit>=$entry && $entry>=$cdate){
+		while($data=mysqli_fetch_array($result)){
+			$p1=-1;
+			$p2=-2;
+			if($data['entrydate']<$exit){
+				$p1=1;
+			}else{
+				$p1=0;
+			}
+			if($data['exitdate']<$entry){
+				$p2=1;
+			}else{
+				$p2=0;
+			}
+			if($p1!=$p2){
+				$ok=false;
+			}
+		}
+	}else{
+		$ok=false;
+	}
+
+
+
+
+	if($ok){
+		$sql="INSERT INTO booking(entrydate,exitdate,member_id,kilos,total) VALUES('$entry','$exit',$id,0,0)";
+	}else{
+		$sql="SELECT * FROM members";
+	}
 	
-	$sql="INSERT INTO booking(entrydate,exitdate,member_id,kilos,total) VALUES('$entry','$exit',$id,0,0)";
 	
-	if(mysqli_query($link,$sql)){
+	if(mysqli_query($link,$sql) && $ok == true){
 		$id=$_SESSION['memberID'];
 		header("Location:bookings.php?insert=yes");
 		} else {
